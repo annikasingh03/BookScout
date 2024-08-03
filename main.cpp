453c7c0a677f374cee26f8a01776b0f4151c4d4b
annikasingh03 <113479983+annikasingh03@users.noreply.github.com>
2024-08-02 20:55:37 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,143 +1,198 @@
-// Angel Maldonado // BOYER MOORE ALGORITHM
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <chrono>
 using namespace std;
-#define NO_OF_CHARS 256
 
-
-void badCharHeuristic(string str, int size, int badchar[NO_OF_CHARS]) {
-    for (int i = 0; i < NO_OF_CHARS; i++)
-        badchar[i] = -1;
-
-    for (int i = 0; i < size; i++)
-        badchar[(int)str[i]] = i;
-}
-
-int search(string txt, string pat) {
-    int m = pat.size();
-    int n = txt.size();
-    int badchar[NO_OF_CHARS];
-    badCharHeuristic(pat, m, badchar);
-
-    int count = 0;
-    int s = 0;
-    while (s <= (n - m)) {
-        int j = m - 1;
-
-        while (j >= 0 && pat[j] == txt[s + j])
-            j--;
-
-        if (j < 0) {
-            if ((s == 0 || !isalpha(txt[s - 1])) && (s + m == n || !isalpha(txt[s + m]))) {
-                count++;
-            }
-            s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
-        } else {
-            s += max(1, j - badchar[txt[s + j]]);
-        }
-    }
-    return count;
-}
-
-string onlyText(const string &text) {
-    string result;
-    for (char c : text) {
-        if (isalpha(c)) {
-            result += tolower(c);
-//       }
-        } else if(isspace(c)){
-            result += ' ';
-            }
-
-
-    }
-    return result;
-}
-
-vector<string> convertToWords(const string &text) {
-    vector<string> words;
-    istringstream stream(text);
-    string word;
-    while (stream >> word) {
-        words.push_back(word);
-    }
-    return words;
-}
-
-map<string, int> countNumWord(const string &text, const vector<string> &words) {
-    map<string, int> wordCount;
-    for (const string &word : words) {
-        //change here for different word size count!!
-        if (word.size() > 6) {
-            wordCount[word] = search(text, word);
-        }
-    }
-    return wordCount;
-}
-bool sortPairs(const pair<string, int> &a, const pair<string, int> &b) {
-    if (a.second > b.second) {
-        return true;
-    } else {
-        return false;
-    }
-}
+using namespace sf;
 
 int main() {
+    RenderWindow window(sf::VideoMode(1000, 1000), "BookScout");
 
-    string filePath = "C:/Users/maldo/CLionProjects/Project3/Alice.txt";
-
-    ifstream file(filePath);
-    if (!file.is_open()) {
-        cout << "Failed to open the file: " << filePath << endl;
-        return 1;
+    // Load the font from the specified path
+    Font font;
+    if (!font.loadFromFile("../font.ttf")) {
+        std::cerr << "Error: Could not load the font file." << std::endl;
+        return -1; // Exit if the font cannot be loaded
+    } else {
+        std::cout << "font loaded" << std::endl;
     }
 
-    stringstream buffer;
-    buffer << file.rdbuf();
-    string text = buffer.str();
-    file.close();
+    // Create a text object
+    Text text;
+    text.setFont(font);
+    text.setString("Welcome to BookScout. Click on any book!");
+    text.setCharacterSize(25); 
+    text.setFillColor(sf::Color::Black); 
 
-    string onlyText1 = onlyText(text);
+    // Center text at the top of the window
+    FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width / 2.0f,
+                   textRect.top + textRect.height / 2.0f);
+    text.setPosition(sf::Vector2f(1000 / 2.0f, textRect.height + 40)); // Offset by 30 pixels from the top
 
-    vector<string> words = convertToWords(onlyText1);
+    // Load the images into textures
+    Texture texture, b1, b2, b3, b4, b5, open_book, km, bm;
+    if (!texture.loadFromFile("images\\tiny_lib.png") ||
+        !b1.loadFromFile("images\\frank.png") ||
+        !b2.loadFromFile("images\\gatsby.png") ||
+        !b3.loadFromFile("images\\treasure.png") ||
+        !b4.loadFromFile("images\\alice.png") ||
+        !b5.loadFromFile("images\\jane.png") ||
+        !open_book.loadFromFile("images\\open_book.png") ||
+        !km.loadFromFile("images\\knuth.png") ||
+        !bm.loadFromFile("images\\boyer.png")) {
+        cerr << "Error: Could not load an image file." << endl;
+        return -1;
+    }
 
-    auto start = chrono::high_resolution_clock::now();
+    // Load sprites
+    Sprite sprite(texture), frank(b1), gatsby(b2), treasure(b3), alice(b4), jane(b5), ob(open_book), knuth(km), boyer(bm);
 
-    map<string, int> numOfWords = countNumWord(onlyText1, words);
+    // Sprite pos
+    FloatRect spriteRect = sprite.getLocalBounds();
+    sprite.setOrigin(spriteRect.left + spriteRect.width / 2.0f,
+                     spriteRect.top + spriteRect.height / 2.0f);
+    sprite.setPosition(Vector2f(1000 / 2.0f, 1000 / 2.0f)); // center in the window
 
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double> duration = end - start;
+    // frankenstein location
+    FloatRect frankRect = frank.getLocalBounds();
+    frank.setOrigin(frankRect.left + frankRect.width / 2.0f,
+                    frankRect.top + frankRect.height / 2.0f);
+    frank.setPosition(Vector2f(1000 / 2.0f - 68, 1000 / 2.0f - 58)); //x axis, y  //subtraction goes left addition goes right
 
-    // Sort
-    vector<pair<string, int>> sortedWords(numOfWords.begin(), numOfWords.end());
-    sort(sortedWords.begin(), sortedWords.end(), sortPairs);
+    // gatsby location
+    FloatRect gatsbyRect = gatsby.getLocalBounds();
+    gatsby.setOrigin(gatsbyRect.left + gatsbyRect.width / 2.0f,
+                     frankRect.top + gatsbyRect.height / 2.0f);
+    gatsby.setPosition(Vector2f(1000 / 2.0f - 43, 1000 / 2.0f - 63)); //x axis, y  //subtraction by less goes left addition goes right
 
-    set<string> commonWords = {"the", "and", "to", "of", "it", "she", "said", "you", "in", "was", "that", "as", "her", "with", "at", "on", "all", "for"};
+    // treasure location
+    FloatRect treasureRect = treasure.getLocalBounds();
+    treasure.setOrigin(treasureRect.left + treasureRect.width / 2.0f,
+                       treasureRect.top + treasureRect.height / 2.0f);
+    treasure.setPosition(Vector2f(1000 / 2.0f - 17, 1000 / 2.0f - 58)); 
 
-    int topNums = 100;
-    int count = 0;
-    cout << "Top " << topNums << " popular words:" << endl;
-    for (auto &word : sortedWords) {
-        if(commonWords.find(word.first) == commonWords.end()) {
-            cout << word.first << ": " << word.second << endl;
-            count++;
-            if(count == topNums){
-                break;
+    // alice location
+    FloatRect aRect = alice.getLocalBounds();
+    alice.setOrigin(aRect.left + aRect.width / 2.0f,
+                    aRect.top + aRect.height / 2.0f);
+    alice.setPosition(Vector2f(1000 / 2.0f + 9, 1000 / 2.0f - 53)); 
+
+    // jane location
+    FloatRect jRect = jane.getLocalBounds();
+    jane.setOrigin(jRect.left + jRect.width / 2.0f,
+                   jRect.top + jRect.height / 2.0f);
+    jane.setPosition(Vector2f(1000 / 2.0f + 74, 1000 / 2.0f - 63)); 
+
+    // open book location
+    FloatRect oRect = ob.getLocalBounds();
+    ob.setOrigin(oRect.left + oRect.width / 2.0f,
+                 oRect.top + oRect.height / 2.0f);
+    ob.setPosition(Vector2f(1000 / 2.0f, 1000 / 2.0f - 63)); 
+
+    // knuth button location
+    FloatRect kmRect = knuth.getLocalBounds();
+    knuth.setOrigin(kmRect.left + kmRect.width / 2.0f,
+                    oRect.top + oRect.height / 2.0f);
+    knuth.setPosition(Vector2f(1000 / 2.0f + 240, 1000 / 2.0f + 350)); 
+
+    // boyer button location
+    FloatRect bmRect = boyer.getLocalBounds();
+    boyer.setOrigin(bmRect.left + oRect.width / 2.0f,
+                    oRect.top + oRect.height / 2.0f);
+    boyer.setPosition(Vector2f(1000 / 2.0f - 200, 1000 / 2.0f + 350)); 
+    // Boolean to track if book and button are clicked
+    bool bookClicked = false;
+    bool boyerClicked = false, knuthClicked = false;
+
+    Text how;
+    how.setFont(font);
+    how.setString("How would you like your BookScout search to be organized?");
+    how.setCharacterSize(30);
+    how.setFillColor(Color::Black);
+
+    // Center the new text
+    FloatRect hTextRect = how.getLocalBounds();
+    how.setOrigin(hTextRect.left + hTextRect.width / 2.0f,
+                  hTextRect.top + hTextRect.height / 2.0f);
+    how.setPosition(Vector2f(1000 / 2.0f, 1000 / 2.0f - 380));
+
+    // Text for showing which button was clicked
+    Text clickedText;
+    clickedText.setFont(font);
+    clickedText.setCharacterSize(25);
+    clickedText.setFillColor(Color::Black);
+
+    // Main loop that continues until the window is closed
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            // Close window on 'close' event or when the Escape key is pressed
+            if (event.type == Event::Closed || 
+                (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)) {
+                window.close();
+            }
+
+            // Check for mouse click events
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+
+                    // Check if the mouse clicked on any book
+                    if (frank.getGlobalBounds().contains(mousePos) ||
+                        gatsby.getGlobalBounds().contains(mousePos) ||
+                        treasure.getGlobalBounds().contains(mousePos) ||
+                        alice.getGlobalBounds().contains(mousePos) ||
+                        jane.getGlobalBounds().contains(mousePos)) {
+                        bookClicked = true;
+                    }
+
+                    // Check if the mouse clicked on Boyer-Moore or Knuth-Morris button
+                    if (bookClicked) {
+                        if (boyer.getGlobalBounds().contains(mousePos)) {
+                            boyerClicked = true;
+                            knuthClicked = false;
+                        } else if (knuth.getGlobalBounds().contains(mousePos)) {
+                            knuthClicked = true;
+                            boyerClicked = false;
+                        }
+                    }
+                }
             }
         }
-    }
-//    cout << sortedWords[i].first << ": " << sortedWords[i].second << endl;
 
-    cout << "Time taken: " << duration.count() << " seconds" << endl;
+        // Clear the window with a white color
+        window.clear(Color::White);
+
+        // Draw the appropriate screen based on clicks
+        if (bookClicked) {
+            if (boyerClicked) {
+                clickedText.setString("Boyer-Moore button clicked!");
+                clickedText.setPosition(Vector2f(1000 / 2.0f - 190, 1000 / 2.0f - 320));
+                window.draw(clickedText);
+            } else if (knuthClicked) {
+                clickedText.setString("Knuth-Morris button clicked!");
+                clickedText.setPosition(Vector2f(1000 / 2.0f - 190 , 1000 / 2.0f - 320)); //subtract by more if you want to go up more
+                window.draw(clickedText);
+            } else {
+                window.draw(how);
+                window.draw(ob);
+                window.draw(boyer);
+                window.draw(knuth);
+            }
+        } else {
+            // Draw the original screen with the books and text
+            window.draw(sprite);
+            window.draw(frank);
+            window.draw(gatsby);
+            window.draw(treasure);
+            window.draw(alice);
+            window.draw(jane);
+            window.draw(text);
+        }
+
+        // Display the current contents of the window
+        window.display();
+    }
 
     return 0;
 }
-
-//30
